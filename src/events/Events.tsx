45b72638d18dad85d8a6807a4a22aa5593df36c2
Gabriel-Villalba <105971696+Gabriel-Villalba@users.noreply.github.com>
2024-11-28 11:44:58 -0300

    color footer

--- src/events/Events.tsx
+++ src/events/Events.tsx
@@ -9,12 +9,12 @@ const Inscripciones: React.FC = () => {
   return (
     <div
       className="relative bg-cover bg-center"
-      style={{ backgroundImage: `url(${fondoNavidad})`, height: '70vh', width:'100vw' }}
+      style={{ backgroundImage: `url(${fondoNavidad})`, height: '80vh', width:'100vw' }}
     >
       <div className="container mx-auto py-8 px-4">
         <div className="container mx-auto px-4">
           <div className="instructivo text-center my-4">
-            <h1 className="text-2xl md:text-3xl font-bold text-center mb-8 text-white">Reserva tu lugar!!!</h1>
+            <h1 className="text-2xl md:text-3xl font-bold text-center mb-8 text-white">INSCRIBITE!!!</h1>
           </div>
           <div className="funciones flex flex-col items-center gap-4 my-4">
             <div className="containerLink bg-blue-500 text-white p-4 rounded shadow hover:bg-blue-600 w-full max-w-md">
@@ -34,22 +34,6 @@ const Inscripciones: React.FC = () => {
             </div>
           </div>
         </div>
-        {/* <Slider {...settings}>
-          {events.map((event) => (
-            <div key={event.id} className="px-2">
-              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                <img src={event.image} alt={event.title} className="w-full h-48 md:h-64 object-cover" />
-                <div className="p-4 md:p-6">
-                  <h3 className="text-lg md:text-xl font-semibold mb-2">{event.title}</h3>
-                  <p className="text-gray-600 mb-4">{event.description}</p>
-                  <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300 w-full md:w-auto">
-                    Inscribite
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Slider> */}
       </div>
     </div>
   );
--- src/footer/component/Footer.tsx
+++ src/footer/component/Footer.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaFacebook, FaInstagram, FaYoutube, FaMapMarker, FaEnvelope, FaWhatsapp } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => setIsMobile(window.innerWidth <= 768);
+    checkIsMobile();
+    window.addEventListener('resize', checkIsMobile);
+    return () => window.removeEventListener('resize', checkIsMobile);
+  }, []);
+
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-black text-white py-8"
+>
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between items-center">
           <div className="w-full md:w-1/3 text-center md:text-left mb-4 md:mb-0">
-            <h3 className="text-lg font-semibold mb-2">CFC Rafaela</h3>
-            <p>Una iglesia para toda la familia</p>
+            <h3 className="text-lg font-semibold mb-2 md:text-lg text-sm">CFC Rafaela</h3>
+            <p className="md:text-base text-xs">Una iglesia para toda la familia</p>
           </div>
           <div className="w-full md:w-1/3 text-center mb-4 md:mb-0">
-            <h4 className="text-lg font-semibold mb-2">Contáctanos</h4>
+            <h4 className="text-lg font-semibold mb-2 md:text-lg text-sm">Contáctanos</h4>
             <p>
               <a href="mailto:cfcoficinarafaela@gmail.com" className="hover:text-blue-400">
                 <FaEnvelope className="inline-block" size={30} />
               </a>
             </p>
             <p>
-              <a href="https://web.whatsapp.com/send?phone=3492704952" target="_blank" className="hover:text-green-500">
-                <FaWhatsapp className="inline-block" size={30} />
-              </a>
+              {isMobile ? (
+                <a href="https://wa.me/3492704952" target="_blank" className="hover:text-green-500">
+                  <FaWhatsapp className="inline-block" size={30} />
+                </a>
+              ) : (
+                <a href="https://web.whatsapp.com/send?phone=3492704952" target="_blank" className="hover:text-green-500">
+                  <FaWhatsapp className="inline-block" size={30} />
+                </a>
+              )}
             </p>
             <p><FaMapMarker className="inline-block" size={30} /> Av. Williner 350, Rafaela</p>
           </div>
           <div className="w-full md:w-1/3 text-center md:text-right">
-            <h4 className="text-lg font-semibold mb-2">Síguenos</h4>
+            <h4 className="text-lg font-semibold mb-2 md:text-lg text-sm">Síguenos</h4>
             <div className="flex justify-center md:justify-end space-x-4">
               <a href="#" className="hover:text-blue-400"><FaFacebook size={30} /></a>
               <a href="https://www.instagram.com/cfcrafaela/?hl=es" target="_blank" className="hover:text-pink-400"><FaInstagram size={30} /></a>
