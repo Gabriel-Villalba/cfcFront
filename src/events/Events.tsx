8b946c21f03d1cd22ef737fc0d30b56cc891782f
Gabriel-Villalba <105971696+Gabriel-Villalba@users.noreply.github.com>
2024-11-29 14:49:34 -0300

    cambios de frases

--- src/events/Events.tsx
+++ src/events/Events.tsx
@@ -14,23 +14,23 @@ const Inscripciones: React.FC = () => {
       <div className="container mx-auto py-8 px-4">
         <div className="container mx-auto px-4">
           <div className="instructivo text-center my-4">
-            <h1 className="text-2xl md:text-3xl font-bold text-center mb-8 text-white">INSCRIBITE!!!</h1>
+            <h1 className="text-2xl md:text-3xl font-bold text-center mb-8 text-white">¡Elegi la función e inscribite!</h1>
           </div>
           <div className="funciones flex flex-col items-center gap-4 my-4">
             <div className="containerLink bg-blue-500 text-white p-4 rounded shadow hover:bg-blue-600 w-full max-w-md">
-              <a href="https://forms.gle/vSF6ggLbKqtAsQ7H7" target="_blank" className="block text-center">Función sábado 20hs</a>
+              <a href="https://forms.gle/vSF6ggLbKqtAsQ7H7" target="_blank" className="block text-center">Función Sábado 14 - 20 hs</a>
             </div>
           </div>
           <br></br>
           <div className="funciones flex flex-col items-center gap-4 my-4">
             <div className="containerLink bg-blue-500 text-white p-4 rounded shadow hover:bg-blue-600 w-full max-w-md">
-              <a href="https://forms.gle/nqct2d7qZUisicKq8" target="_blank" className="block text-center">Función domingo 18hs</a>
+              <a href="https://forms.gle/nqct2d7qZUisicKq8" target="_blank" className="block text-center">Función Domingo 15 - 18 hs</a>
             </div>
           </div>
           <br></br>
           <div className="funciones flex flex-col items-center gap-4 my-4">
             <div className="containerLink bg-blue-500 text-white p-4 rounded shadow hover:bg-blue-600 w-full max-w-md">
-              <a href="https://forms.gle/m9RGCiGwzchmoFjE8" target="_blank" className="block text-center">Función domingo 20hs</a>
+              <a href="https://forms.gle/m9RGCiGwzchmoFjE8" target="_blank" className="block text-center">Función Domingo 15 - 20 hs</a>
             </div>
           </div>
         </div>
