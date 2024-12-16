7d1f68a4620411cc94313da43db7372286c12c52
Gabriel-Villalba <105971696+Gabriel-Villalba@users.noreply.github.com>
2024-12-15 21:16:13 -0300

    changes Home

--- src/home/component/Home.tsx
+++ src/home/component/Home.tsx
@@ -1,49 +1,34 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import imageDesktop from "../img/fondoDesktop.png";
-import imageMobile from "../img/fondoMobil.jpeg";
+import React from 'react';
+import imageDesktop from "../img/PLACA DE BIENVENIDA.png";
+import imageMobil from "../img/PLACA DE BIENVENIDA.png";
+import '../../public/fonts/fonts.css';
 
 const Home: React.FC = () => {
-  useEffect(() => {
-    window.scrollTo(0, 80); // Desplaza la página a la parte superior al cargar el componente
-  }, []);
-
-  const navigate = useNavigate();
-
-  const handleButtonClick = () => {
-    navigate('/eventos');
-  };
-
   return (
-    <div className="relative flex-grow flex items-center justify-center mt-0" style={{ height: '100vh', width: '100%', overflow: 'hidden' }}>
+    <div className="relative min-h-screen flex items-center justify-center">
+      {/* Imagen de escritorio */}
       <div
-        className="hidden md:block absolute inset-0 bg-cover bg-center z-0"
-        style={{ backgroundImage: `url(${imageDesktop})`, width: '100%', height: '100%', objectFit: 'cover', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
-      ></div>
-      <div
-        className="block md:hidden absolute inset-0 bg-cover bg-center z-0"
-        style={{ backgroundImage: `url(${imageMobile})`, width: '100%', height: '100%', objectFit: 'cover', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
-      ></div>
-      <button
-        onClick={handleButtonClick}
-        className="absolute bottom-10 left-2 bg-blue-600 text-white p-4 rounded shadow hover:text-blue-200 max-w-md"
+        className="absolute inset-0 bg-cover bg-center z-0 hidden md:block"
         style={{
-          backgroundColor: '#2563EB', // bg-blue-600
-          opacity: '1',               // full opacity
-          color: 'white',             // text-white
-          padding: '1rem',            // p-4
-          borderRadius: '0.45rem',    // rounded
-          boxShadow: '0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.06)', // shadow
-          width: '95%',               // w-full
-          maxWidth: '28rem',          // max-w-md
-          transition: 'background-color 0.3s ease', 
-          fontSize: '1.25rem',
+          backgroundImage: `url(${imageDesktop})`,
         }}
-        onMouseEnter={(e) => e.currentTarget.style.backgroundColor = '#1E40AF'}  // hover:bg-blue-700
-        onMouseLeave={(e) => e.currentTarget.style.backgroundColor = '#2563EB'}  // reset on mouse leave
-      >
-        Reserva tu lugar
-      </button>
+      ></div>
+      {/* Imagen para móviles */}
+      <div
+        className="absolute inset-0 bg-cover bg-center z-0 md:hidden"
+        style={{
+          backgroundImage: `url(${imageMobil})`,
+        }}
+      ></div>
+      <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+      <div className="relative z-20 text-center text-white px-4">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 nunito">
+          CFC Rafaela
+        </h1>
+        <p className="text-xl md:text-2xl font-bold lg:text-3xl nunito">
+          Una iglesia para toda la familia
+        </p>
+      </div>
     </div>
   );
 };
